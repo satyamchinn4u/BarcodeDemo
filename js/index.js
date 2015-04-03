8bf89dd283ce1ef4c31cf9a6d776a0b85eea9e0f
satyamchinn4u <satyamchinna4u@gmail.com>
2015-04-03 11:11:24 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -57,25 +57,25 @@ var app = {
         var scanner = cordova.require("cordova/plugin/BarcodeScanner");
 
         scanner.scan( function (result) { 
-
+ 		alert("We got a barcode\n Result: " + result.text);  
 	//	var serviceURL='http://automation.ctrl4c.com/cmsbluetheme/index.php/reports/';
 	//	var url=serviceURL + 'checkcoupons/67235537/460';
         
-		 $.getJSON("http://automation.ctrl4c.com/mobileApp/mobile.php?id=67546", function(barcodeResult){
-        		 //alert(barcodeResult.data);
+		 //$.getJSON("http://automation.ctrl4c.com/mobileApp/mobile.php?id=67546", function(barcodeResult){
+   //     		 //alert(barcodeResult.data);
         
 		
 		
-		   //alert(url);
-		   if(barcodeResult.data=="1")
-		   {
-		   alert('Coupons applied succesfully');
-		   }else
-		   {
-		   alert('Your Coupons has either been expired or invalid');   
-		   }
-		 });
-		    alert("We got a barcode\n Result: " + result.text);  
+		 //  //alert(url);
+		 //  if(barcodeResult.data=="1")
+		 //  {
+		 //  alert('Coupons applied succesfully');
+		 //  }else
+		 //  {
+		 //  alert('Your Coupons has either been expired or invalid');   
+		 //  }
+		 //});
+		   
 
            console.log("Scanner result: \n" +
                 "text: " + result.text + "\n" +
